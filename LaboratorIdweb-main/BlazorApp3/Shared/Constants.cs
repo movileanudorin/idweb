9d9004688dfc80d8e71e2e9fe05c99dae5033074
movileanudorin <62745155+movileanudorin@users.noreply.github.com>
2021-01-17 19:46:11 +0300

    Delete LaboratorIdweb-main directory

--- LaboratorIdweb-main/BlazorApp3/Shared/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BlazorApp3.Shared
-{
-    public static class Constants
-    {
-        public static string transferSuccess = "transferSuccess";
-    }
-}
--- LaboratorIdweb-main/BlazorApp3/Shared/TransactionsHistoryData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlazorApp3.Shared
-{
-    public class TransactionsHistoryData
-    {
-        public TransactionDto[] Transactions { get; set; }
-        public int ItemCount { get; set; }
-    }
-}
